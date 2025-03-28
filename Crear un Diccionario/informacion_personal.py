1b1ffa284a674bf6a7a171e434d3483e7b9a2d88
kimverly bustamante <199150845+kimverlybustamante@users.noreply.github.com>
2025-03-28 15:36:45 -0400

    Subiendo codigo

---
+++ Crear un Diccionario/informacion_personal.py
@@ -0,0 +1,23 @@
+# 1. Crear un diccionario con información ficticia
+informacion_personal = {
+    "nombre": "Laura Sofia",
+    "edad": 25,
+    "ciudad": "Colombia",
+    "profesion": "Periodista"
+}
+
+# 2. Modificar el valor de la clave "ciudad"
+informacion_personal["ciudad"] = "Colombia"
+
+# 3. Agregar una nueva clave-valor al diccionario ("hobby")
+informacion_personal["hobby"] = "Bailar"
+
+# 4. Verificar si la clave "telefono" existe, si no, agregarla
+if "telefono" not in informacion_personal:
+    informacion_personal["telefono"] = "0996547123"
+
+# 5. Eliminar la clave "edad"
+del informacion_personal["edad"]
+
+# 6. Imprimir el diccionario final
+print(informacion_personal)
